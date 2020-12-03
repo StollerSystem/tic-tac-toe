248614a05f247e8bd63e896dc160a397909f9a43
Ben Stoller <stollersystemsound@gmail.com>
2020-12-03 11:39:09 -0800

    finish tests for action creators
    Co-authored-by: <brian.harv3y@gmail.com>
    Co-Authored-By: Michael Watts <wattsjmichael@gmail.com>

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -5,11 +5,25 @@ describe('tic tac toe actions', () => {
   test('add squares should create ADD_SQUARES action', () => {
     const squares = "test";
     const history = "test";
-
     expect(actions.addSquares(squares, history)).toEqual({
       type: 'ADD_SQUARES',
       squares: [{squares: squares}],
       history: history
+    });
+  });
+  
+  test('Step Number should increase by a value', () => {
+    const step = 1;
+
+    expect(actions.stepNumber(step)).toEqual({
+      type: 'STEP_NUMBER',
+      stepNumber: step
+    });
+  });
+
+  test('x is next should create X_IS_NEXT action', () => {
+    expect(actions.xIsNext()).toEqual({
+      type: 'X_IS_NEXT'
     })
   })
-})
+});
